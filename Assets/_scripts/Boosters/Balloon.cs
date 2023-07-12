2b7a5bd0ba2f4731cfed7f36979371d702e9cb10
Jindra Patik <panstratosh@gmail.com>
2023-07-12 05:57:56 +0200

    Balloon delivery

--- Assets/_scripts/Boosters/Balloon.cs
+++ Assets/_scripts/Boosters/Balloon.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _randomYTorqueRadius;
+    [SerializeField] private float _minTorqueRadius, _maxTorqueRadius;
+    [SerializeField] private GameObject _supplyCrate;
+    private ConstantForce _constantForce;
+    private Transform _myCurrentTransform;
+
+
+    
+    private void Start()
     {
-        
+        GetRandomLocalTorque();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void GetRandomLocalTorque()
     {
-        
+        _constantForce = GetComponent<ConstantForce>();
+        _randomYTorqueRadius = Random.Range(_minTorqueRadius, _maxTorqueRadius);
+        Vector3 torque = _constantForce.relativeTorque;
+        torque.y = _randomYTorqueRadius;
+        _constantForce.relativeTorque = torque;
+    }
+
+    private TerrainCollider terrain;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent<TerrainCollider>(out terrain)) 
+        {
+            _myCurrentTransform = transform;
+            Instantiate(_supplyCrate, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
     }
 }
